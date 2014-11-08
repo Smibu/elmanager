0349a0eb6967401872cfc7959ac008a240590959
Smibu <smibu90@gmail.com>
2014-11-08 21:45:52 +0200

    Added LevelException class.

---
+++ LevelException.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Elmanager
+{
+    internal class LevelException : Exception
+    {
+        public LevelException(string message)
+            : base(message)
+        {
+            
+        }
+    }
+}
