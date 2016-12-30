b92c4acecd94acfe11cff0ce891713e3c7177569
Smibu <smibu90@gmail.com>
2016-12-30 23:44:40 +0200

    Add separate executables for replay manager and SLE (closes #93)

---
+++ ReplayManager/RM.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace RM
+{
+    internal static class Launch
+    {
+        public static void Main()
+        {
+            var p = new ProcessStartInfo()
+            {
+                FileName = "Elmanager.exe",
+                Arguments = "/replaymanager"
+            };
+            Process.Start(p);
+        }
+    }
+}
---
+++ SLE/SLE.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+
+namespace SLE
+{
+    internal static class Sle
+    {
+        public static void Main()
+        {
+            var p = new ProcessStartInfo()
+            {
+                FileName = "Elmanager.exe",
+                Arguments = "/leveleditor"
+            };
+            Process.Start(p);
+        }
+    }
+}
