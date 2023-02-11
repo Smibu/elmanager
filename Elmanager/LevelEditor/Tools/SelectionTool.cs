32f1ff6fa459b7d8700a6a38fb4b343e5574f370
Smibu <smibu90@gmail.com>
2023-02-11 17:46:58 +0200

    Fix case

--- Elmanager/LevelEditor/Tools/SelectionTool.cs
+++ Elmanager/LevelEditor/Tools/SelectionTool.cs
@@ -510,7 +510,7 @@ internal class SelectionTool : ToolBase, IEditorTool
 
     public void UpdateHelp()
     {
-        LevEditor.InfoLabel.Text = "LMouse: select level elements; LShift: Bend edge; LShift + click: lock edge angle; LAlt + click: select all inside.";
+        LevEditor.InfoLabel.Text = "LMouse: select level elements; LShift: bend edge; LShift + click: lock edge angle; LAlt + click: select all inside.";
     }
 
     private static void MarkSelectedInArea<T>(ref T z, double selectionxMin, double selectionxMax,
