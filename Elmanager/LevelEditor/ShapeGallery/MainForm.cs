04f7a6c0652fbfcf2096494f38bea2eccc13e34b
Sjurdur <sjurdursan@gmail.com>
2025-02-08 22:45:35 +0100

    Work towards implementing a virtual mode panel for the shape gallery

--- Elmanager/LevelEditor/ShapeGallery/MainForm.cs
+++ Elmanager/LevelEditor/ShapeGallery/MainForm.cs
@@ -1,12 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
+using Elmanager.LevelEditor.ShapeGallery;
 
-namespace Elmanager.LevelEditor.ShapeGallery
+public partial class MainForm : Form
 {
-    internal class MainForm
+    private FlowLayoutPanel flowLayoutPanel;
+    private VScrollBar vScrollBar;
+    private List<CustomShapeControl> reusableControls;
+    private List<string> imageFilePaths;
+    private int itemsPerPage = 9;  // 3 rows × 3 columns
+
+    public MainForm()
     {
+        InitializeComponent();
+        SetupUI();
+        LoadImageFilePaths(@"C:\path\to\your\images");  // Change to your directory
+        SetupReusableControls(9);  // Create 9 reusable user controls
+        SetupScrollBar();
+        UpdateDisplayedControls(0);  // Initialize with the first page
+    }
+
+    private void SetupUI()
+    {
+        flowLayoutPanel = new FlowLayoutPanel
+        {
+            Dock = DockStyle.Fill,
+            WrapContents = true,
+            FlowDirection = FlowDirection.LeftToRight
+        };
+
+        vScrollBar = new VScrollBar
+        {
+            Dock = DockStyle.Right,
+            Width = 20
+        };
+        vScrollBar.ValueChanged += vScrollBar_ValueChanged;
+
+        this.Controls.Add(flowLayoutPanel);
+        this.Controls.Add(vScrollBar);
+    }
+
+    private void LoadImageFilePaths(string directoryPath)
+    {
+        if (Directory.Exists(directoryPath))
+        {
+            imageFilePaths = Directory.EnumerateFiles(directoryPath, "*.jpg")
+                                      .Concat(Directory.EnumerateFiles(directoryPath, "*.png"))
+                                      .Concat(Directory.EnumerateFiles(directoryPath, "*.jpeg"))
+                                      .ToList();
+        }
+        else
+        {
+            MessageBox.Show("Directory not found!");
+            imageFilePaths = new List<string>();
+        }
+    }
+
+    private void SetupReusableControls(int numControls)
+    {
+        reusableControls = new List<CustomShapeControl>();
+
+        for (int i = 0; i < numControls; i++)
+        {
+            var uc = new CustomShapeControl();
+            flowLayoutPanel.Controls.Add(uc);
+            reusableControls.Add(uc);
+        }
+    }
+
+    private void SetupScrollBar()
+    {
+        int totalPages = (int)Math.Ceiling(1.0 * imageFilePaths.Count / itemsPerPage);
+        vScrollBar.Minimum = 0;
+        vScrollBar.Maximum = Math.Max(totalPages - 1, 0);  // Ensure the scrollbar has valid range
+        vScrollBar.LargeChange = 1;
+        vScrollBar.SmallChange = 1;
+    }
+
+    private void vScrollBar_ValueChanged(object sender, EventArgs e)
+    {
+        UpdateDisplayedControls(vScrollBar.Value);
+    }
+
+    private void UpdateDisplayedControls(int startPage)
+    {
+        int startIndex = startPage * itemsPerPage;
+
+        for (int i = 0; i < reusableControls.Count; i++)
+        {
+            int dataIndex = startIndex + i;
+            if (dataIndex < imageFilePaths.Count)
+            {
+                reusableControls[i].Visible = true;
+                reusableControls[i].UpdateContent(imageFilePaths[dataIndex], $"Item {dataIndex}");
+            }
+            else
+            {
+                reusableControls[i].Visible = false;
+            }
+        }
     }
 }
--- Elmanager/LevelEditor/ShapeGallery/VirtualizedFlowPanel.cs
+++ Elmanager/LevelEditor/ShapeGallery/VirtualizedFlowPanel.cs
@@ -1,12 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using Image = System.Drawing.Image;
+using Point = System.Windows.Point;
+using Size = System.Windows.Size;
 
-namespace Elmanager.LevelEditor.ShapeGallery
+namespace Elmanager.LevelEditor.ShapeGallery;
+
+public class VirtualizedFlowPanel : System.Windows.Controls.Panel
 {
-    internal class VirtualizedFlowPanel
+    private int totalItems;
+    private int rows, columns;
+    private List<LevelControl> virtualizedControls;
+    private int startIndex = 0;
+
+    private Func<int, (Image, string)> dataProvider;
+
+    public int ItemHeight { get; set; } = 128;
+    public int ItemWidth { get; set; } = 128;
+
+    public void Initialize(int totalItems, Func<int, (Image, string)> dataProvider, int rows = 3, int columns = 3)
     {
+        this.totalItems = totalItems;
+        this.dataProvider = dataProvider;
+        this.rows = rows;
+        this.columns = columns;
+
+        // Enable scrolling
+        this.AutoScroll = true;
+        this.VerticalScroll.SmallChange = ItemHeight;
+        this.SetScrollBar();
+
+        CreateVirtualControls();
+        UpdateControls();
+    }
+
+    private void SetScrollBar()
+    {
+        int virtualHeight = (totalItems / columns) * ItemHeight;
+        this.AutoScrollMinSize = new Size(0, virtualHeight);
+    }
+
+    private void CreateVirtualControls()
+    {
+        virtualizedControls = new List<ImageLabelControl>();
+
+        for (int i = 0; i < rows * columns; i++)
+        {
+            var control = new ImageLabelControl();
+            control.Size = new Size(ItemWidth, ItemHeight);
+            control.Location = GetControlPosition(i);
+            virtualizedControls.Add(control);
+            this.Controls.Add(control);
+        }
+    }
+
+    private Point GetControlPosition(int index)
+    {
+        int row = index / columns;
+        int col = index % columns;
+        return new Point(col * ItemWidth, row * ItemHeight);
+    }
+
+    private void UpdateControls()
+    {
+        int firstVisibleIndex = VerticalScroll.Value / ItemHeight * columns;
+
+        for (int i = 0; i < virtualizedControls.Count; i++)
+        {
+            int dataIndex = firstVisibleIndex + i;
+            if (dataIndex < totalItems)
+            {
+                var (image, text) = dataProvider(dataIndex);  // Get data from the provider
+                virtualizedControls[i].Visible = true;
+                virtualizedControls[i].Location = GetControlPosition(dataIndex - firstVisibleIndex);
+                virtualizedControls[i].UpdateContent(image, text);
+            }
+            else
+            {
+                virtualizedControls[i].Visible = false;
+            }
+        }
+    }
+
+    protected override void OnScroll(ScrollEventArgs se)
+    {
+        base.OnScroll(se);
+        UpdateControls();
     }
 }
