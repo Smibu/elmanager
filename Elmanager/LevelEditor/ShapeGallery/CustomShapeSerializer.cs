a392fa9ebdef3a0d0e2f360961bb18ba18a33274
Sjurdur <sjurdursan@gmail.com>
2025-02-14 22:39:56 +0100

    Normalize positions by using level bounds after having removed start from the loaded shape

--- Elmanager/LevelEditor/ShapeGallery/CustomShapeSerializer.cs
+++ Elmanager/LevelEditor/ShapeGallery/CustomShapeSerializer.cs
@@ -21,32 +21,34 @@ internal static class CustomShapeSerializer
                 throw new InvalidOperationException("Cannot load across level shapes.");
             }
 
+            level.Objects = level.Objects.Where(o => o.Type != ObjectType.Start).ToList(); // Remove start object
+            level.UpdateBounds();
+
             //// Cannot use Bounds since it relies on the Start Object, and we are default start position to (0, 0) in shapes
-            // Vector centerByBounds = new Vector((level.Bounds.XMin + level.Bounds.XMax) / 2, (level.Bounds.YMin + level.Bounds.YMax) / 2);
+            Vector centerByBounds = new Vector((level.Bounds.XMin + level.Bounds.XMax) / 2, (level.Bounds.YMin + level.Bounds.YMax) / 2);
 
             // Normalize positions on
             List<Polygon> levelPolygons = level.Polygons;
-            List<LevObject> levObjects = level.Objects.Where(o => o.Type != ObjectType.Start).ToList();
+            List<LevObject> levObjects = level.Objects; // No need to filter start object since it is already removed
             List<GraphicElement> levelGraphicElements = level.GraphicElements;
-            var (center, _, _) = GeometryUtils.CalculateBoundingBox(levelPolygons, levObjects, levelGraphicElements);
 
             // Normalize positions
             foreach (var polygon in levelPolygons)
             {
                 for (int i = 0; i < polygon.Vertices.Count; i++)
                 {
-                    polygon.Vertices[i] = new Vector(polygon.Vertices[i].X - center.X, polygon.Vertices[i].Y - center.Y);
+                    polygon.Vertices[i] = new Vector(polygon.Vertices[i].X - centerByBounds.X, polygon.Vertices[i].Y - centerByBounds.Y);
                 }
             }
 
             foreach (var obj in levObjects)
             {
-                obj.Position = new Vector(obj.Position.X - center.X, obj.Position.Y - center.Y);
+                obj.Position = new Vector(obj.Position.X - centerByBounds.X, obj.Position.Y - centerByBounds.Y);
             }
 
             foreach (var graphicElement in levelGraphicElements)
             {
-                graphicElement.Position = new Vector(graphicElement.Position.X - center.X, graphicElement.Position.Y - center.Y);
+                graphicElement.Position = new Vector(graphicElement.Position.X - centerByBounds.X, graphicElement.Position.Y - centerByBounds.Y);
             }
 
 
