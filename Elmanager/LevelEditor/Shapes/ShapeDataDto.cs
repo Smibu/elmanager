86298e6df335a87ad6a8f3b395994d92d58f834b
Sjurdur <sjurdursan@gmail.com>
2025-02-22 18:30:50 +0100

    Remove no longer used fields

--- Elmanager/LevelEditor/Shapes/ShapeDataDto.cs
+++ Elmanager/LevelEditor/Shapes/ShapeDataDto.cs
@@ -1,17 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Elmanager.Lev;
-using Elmanager.Rendering;
+﻿using Elmanager.Lev;
 
 namespace Elmanager.LevelEditor.Shapes;
 
 internal class ShapeDataDto(Level level)
 {
     public Level Level { get; set; } = level;
-
-    public List<Polygon> Polygons { get; set; } = level.Polygons;
-
-    public List<LevObject> Objects { get; set; } = level.Objects.Where(o => o.Type != ObjectType.Start).ToList();
-
-    public List<GraphicElement> GraphicElements { get; set; } = level.GraphicElements;
 }
