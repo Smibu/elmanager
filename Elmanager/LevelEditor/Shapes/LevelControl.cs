d6833991ea6d75df0de6c1f7a90b5426621f14b9
Sjurdur <sjurdursan@gmail.com>
2025-02-22 18:26:18 +0100

    Convert to file-scoped namespace to be consistent with the rest of the code

--- Elmanager/LevelEditor/Shapes/LevelControl.cs
+++ Elmanager/LevelEditor/Shapes/LevelControl.cs
@@ -8,191 +8,190 @@ using OpenTK.GLControl;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Windowing.Common;
 
-namespace Elmanager.LevelEditor.Shapes
+namespace Elmanager.LevelEditor.Shapes;
+
+public class LevelControl : GLControl
 {
-    public class LevelControl : GLControl
+    private ElmaRenderer _renderer;
+    private readonly ElmaRenderer _originalElmaRenderer;
+    private Level? _level;
+    private ElmaCamera? _camera;
+    private readonly RenderingSettings _renderingSettings;
+    private readonly SceneSettings _sceneSettings;
+    private ZoomController? _zoomController;
+    private bool _isFirstRender = true;
+    public bool DisableRendering { get; set; } = true;
+
+    private readonly DispatcherTimer _resizeTimer;
+    private const int DebounceInterval = 6; // Debounce interval in milliseconds
+
+    internal LevelControl(GLControl sharedContext, SceneSettings sceneSettings, RenderingSettings renderingSettings, ElmaRenderer elmaRenderer, Level? level=null) :
+        base(new GLControlSettings {
+            AutoLoadBindings = false,
+            Profile = ContextProfile.Compatability
+        })
     {
-        private ElmaRenderer _renderer;
-        private readonly ElmaRenderer _originalElmaRenderer;
-        private Level? _level;
-        private ElmaCamera? _camera;
-        private readonly RenderingSettings _renderingSettings;
-        private readonly SceneSettings _sceneSettings;
-        private ZoomController? _zoomController;
-        private bool _isFirstRender = true;
-        public bool DisableRendering { get; set; } = true;
+        _renderingSettings = renderingSettings;
 
-        private readonly DispatcherTimer _resizeTimer;
-        private const int DebounceInterval = 6; // Debounce interval in milliseconds
-
-        internal LevelControl(GLControl sharedContext, SceneSettings sceneSettings, RenderingSettings renderingSettings, ElmaRenderer elmaRenderer, Level? level=null) :
-            base(new GLControlSettings {
-                AutoLoadBindings = false,
-                Profile = ContextProfile.Compatability
-            })
+        if (level != null)
         {
-            _renderingSettings = renderingSettings;
-
-            if (level != null)
-            {
-                _level = level;
-                _camera = new ElmaCamera();
-                _zoomController = new ZoomController(_camera, _level, () => RedrawScene());
-            }
-
-            _sceneSettings = sceneSettings;
-            _originalElmaRenderer = elmaRenderer;
-
-            _renderer = elmaRenderer;
-
-            this.Load += LevelControl_Load;
-
-            if (!IsHandleCreated)
-            {
-                SharedContext = sharedContext; // Set shared context before initialization
-            }
-
-            // Initialize the resize timer
-            _resizeTimer = new DispatcherTimer
-            {
-                Interval = TimeSpan.FromMilliseconds(DebounceInterval)
-            };
-            _resizeTimer.Tick += ResizeTimer_Tick;
+            _level = level;
+            _camera = new ElmaCamera();
+            _zoomController = new ZoomController(_camera, _level, () => RedrawScene());
         }
 
-        /**
-         * Set the level to be displayed in the control
-         * To disable rendering, set level to null
-         */
-        internal void SetLevel(Level? level)
+        _sceneSettings = sceneSettings;
+        _originalElmaRenderer = elmaRenderer;
+
+        _renderer = elmaRenderer;
+
+        this.Load += LevelControl_Load;
+
+        if (!IsHandleCreated)
         {
-            if (level != null)
-            {
-                _level = level;
-                _camera = new ElmaCamera();
-                _zoomController = new ZoomController(_camera, _level, () => RedrawScene());
-                _isFirstRender = true;
-            }
-            else
-            {
-                // Refactor to something more elegant
-                _level = null;
-            }
+            SharedContext = sharedContext; // Set shared context before initialization
         }
 
-        protected override void OnHandleCreated(EventArgs e)
+        // Initialize the resize timer
+        _resizeTimer = new DispatcherTimer
         {
-            base.OnHandleCreated(e);
+            Interval = TimeSpan.FromMilliseconds(DebounceInterval)
+        };
+        _resizeTimer.Tick += ResizeTimer_Tick;
+    }
 
-            if (Context != null && !Context.IsCurrent)
-            {
-                MakeCurrent();
-            }
+    /**
+     * Set the level to be displayed in the control
+     * To disable rendering, set level to null
+     */
+    internal void SetLevel(Level? level)
+    {
+        if (level != null)
+        {
+            _level = level;
+            _camera = new ElmaCamera();
+            _zoomController = new ZoomController(_camera, _level, () => RedrawScene());
+            _isFirstRender = true;
+        }
+        else
+        {
+            // Refactor to something more elegant
+            _level = null;
+        }
+    }
 
+    protected override void OnHandleCreated(EventArgs e)
+    {
+        base.OnHandleCreated(e);
+
+        if (Context != null && !Context.IsCurrent)
+        {
+            MakeCurrent();
+        }
+
+        GL.Viewport(0, 0, Width, Height); // Set viewport to the entire control
+        if (Context != null)
+            Context.SwapInterval = 0;
+    }
+
+    private void LevelControl_Load(object? sender, EventArgs e)
+    {
+        // Initialization code for OpenGL
+        _renderer = new ElmaRenderer(this, _renderingSettings);
+        _renderer.OpenGlLgr = _originalElmaRenderer.OpenGlLgr; // Slightly faster with these lines. There is however likely a memory leak here
+        _renderer._lgrCache = _originalElmaRenderer._lgrCache; // Slightly faster with these lines. There is however likely a memory leak here
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+        base.OnPaint(e);
+        Render(false);
+    }
+
+    private void Render(bool resetViewport)
+    {
+        if (DisableRendering)
+        {
+            return;
+        }
+
+        if (Context == null)
+        {
+            return;
+        }
+
+        if (!Context.IsCurrent)
+        {
+            MakeCurrent();
+        }
+
+        GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+        CheckGLError("GL.Clear");
+
+        if (_isFirstRender && _level != null)
+        {
             GL.Viewport(0, 0, Width, Height); // Set viewport to the entire control
-            if (Context != null)
-                Context.SwapInterval = 0;
+
+            _renderer.UpdateSettings(_level, _renderingSettings);
+            _renderer.InitializeLevel(_level, _renderingSettings);
+            _level.UpdateBounds();
+            _zoomController?.ZoomFill(_renderingSettings);
+            _isFirstRender = false;
         }
 
-        private void LevelControl_Load(object? sender, EventArgs e)
+        if (resetViewport)
         {
-            // Initialization code for OpenGL
-            _renderer = new ElmaRenderer(this, _renderingSettings);
-            _renderer.OpenGlLgr = _originalElmaRenderer.OpenGlLgr; // Slightly faster with these lines. There is however likely a memory leak here
-            _renderer._lgrCache = _originalElmaRenderer._lgrCache; // Slightly faster with these lines. There is however likely a memory leak here
+            GL.Viewport(0, 0, Width, Height); // Set viewport to the entire control
+            _zoomController?.ZoomFill(_renderingSettings);
         }
 
-        protected override void OnPaint(PaintEventArgs e)
+        // Use the ElmaRenderer to render the level
+        RedrawScene();
+
+        SwapBuffers();
+    }
+
+    internal void RedrawScene(object? sender = null, EventArgs? e = null)
+    {
+        if (_level != null && _camera != null)
         {
-            base.OnPaint(e);
-            Render(false);
+            _renderer.DrawScene(_level, _camera, _sceneSettings, _renderingSettings);
         }
+    }
 
-        private void Render(bool resetViewport)
+    private static void CheckGLError(string location)
+    {
+        ErrorCode error = GL.GetError();
+        if (error != ErrorCode.NoError)
         {
-            if (DisableRendering)
-            {
-                return;
-            }
-
-            if (Context == null)
-            {
-                return;
-            }
-
-            if (!Context.IsCurrent)
-            {
-                MakeCurrent();
-            }
-
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            CheckGLError("GL.Clear");
-
-            if (_isFirstRender && _level != null)
-            {
-                GL.Viewport(0, 0, Width, Height); // Set viewport to the entire control
-
-                _renderer.UpdateSettings(_level, _renderingSettings);
-                _renderer.InitializeLevel(_level, _renderingSettings);
-                _level.UpdateBounds();
-                _zoomController?.ZoomFill(_renderingSettings);
-                _isFirstRender = false;
-            }
-
-            if (resetViewport)
-            {
-                GL.Viewport(0, 0, Width, Height); // Set viewport to the entire control
-                _zoomController?.ZoomFill(_renderingSettings);
-            }
-
-            // Use the ElmaRenderer to render the level
-            RedrawScene();
-
-            SwapBuffers();
+            System.Diagnostics.Debug.WriteLine($"OpenGL Error at {location}: {error}");
         }
+    }
 
-        internal void RedrawScene(object? sender = null, EventArgs? e = null)
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
         {
-            if (_level != null && _camera != null)
-            {
-                _renderer.DrawScene(_level, _camera, _sceneSettings, _renderingSettings);
-            }
+            // Dispose of any disposable resources here
+            //_renderer.Dispose(); // Crashes.
         }
+        base.Dispose(disposing);
+    }
 
-        private static void CheckGLError(string location)
-        {
-            ErrorCode error = GL.GetError();
-            if (error != ErrorCode.NoError)
-            {
-                System.Diagnostics.Debug.WriteLine($"OpenGL Error at {location}: {error}");
-            }
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                // Dispose of any disposable resources here
-                //_renderer.Dispose(); // Crashes.
-            }
-            base.Dispose(disposing);
-        }
-
-        protected override void OnResize(EventArgs e)
-        {
-            base.OnResize(e);
+    protected override void OnResize(EventArgs e)
+    {
+        base.OnResize(e);
             
-            // Restart the debounce timer
-            _resizeTimer.Stop();
-            _resizeTimer.Start();
-        }
+        // Restart the debounce timer
+        _resizeTimer.Stop();
+        _resizeTimer.Start();
+    }
 
-        private void ResizeTimer_Tick(object? sender, EventArgs e)
-        {
-            // Stop the timer
-            _resizeTimer.Stop();
+    private void ResizeTimer_Tick(object? sender, EventArgs e)
+    {
+        // Stop the timer
+        _resizeTimer.Stop();
 
-            Render(true);
-        }
+        Render(true);
     }
 }
