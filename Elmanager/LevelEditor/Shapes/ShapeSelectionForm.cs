5ace6523d06be5df373ba54d0d6abb40f184b662
Sjurdur <sjurdursan@gmail.com>
2025-03-01 05:46:22 +0100

    Add nuget package for handling NaturalSort

--- Elmanager/LevelEditor/Shapes/ShapeSelectionForm.cs
+++ Elmanager/LevelEditor/Shapes/ShapeSelectionForm.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using Elmanager.IO;
 using Elmanager.Lev;
 using Path = System.IO.Path;
+using NaturalSort.Extension;
 
 namespace Elmanager.LevelEditor.Shapes;
 
@@ -231,7 +232,7 @@ internal partial class ShapeSelectionForm : Form
         // Load shapes from the specified folder
         _shapes = Directory.GetFiles(folderPath, "*.lev")
             .Select(filePath => (Name: Path.GetFileNameWithoutExtension(filePath), ShapePath: filePath))
-            .OrderBy(shape => shape.Name, new CustomComparer<string>(CompareNatural))
+            .OrderBy(shape => shape.Name, StringComparison.OrdinalIgnoreCase.WithNaturalSort())
             .ToList();
 
         SetupScrollBar(_shapes.Count);
@@ -408,7 +409,7 @@ internal partial class ShapeSelectionForm : Form
             .SelectMany(subfolder => Directory.GetFiles(subfolder, "*.lev")
                 .Select(filePath => (Name: Path.GetFileNameWithoutExtension(filePath), ShapePath: filePath, Subfolder: Path.GetFileName(subfolder))))
             .OrderBy(shape => shape.Subfolder)
-            .ThenBy(shape => shape.Name, new CustomComparer<string>(CompareNatural))
+            .ThenBy(shape => shape.Name, StringComparison.OrdinalIgnoreCase.WithNaturalSort())
             .Select(shape => (shape.Name, shape.ShapePath))
             .ToList();
 
@@ -426,153 +427,6 @@ internal partial class ShapeSelectionForm : Form
 
             // Reuse the existing OK button click handler to close the dialog
             ButtonOk_Click(this, e);
-        }
-    }
-
-    // Taken from
-    // https://stackoverflow.com/a/7048016
-    public static int CompareNatural(string strA, string strB)
-    {
-        return CompareNatural(strA, strB, CultureInfo.CurrentCulture, CompareOptions.IgnoreCase);
-    }
-
-    public static int CompareNatural(string strA, string strB, CultureInfo culture, CompareOptions options)
-    {
-        CompareInfo cmp = culture.CompareInfo;
-        int iA = 0;
-        int iB = 0;
-        int softResult = 0;
-        int softResultWeight = 0;
-        while (iA < strA.Length && iB < strB.Length)
-        {
-            bool isDigitA = Char.IsDigit(strA[iA]);
-            bool isDigitB = Char.IsDigit(strB[iB]);
-            if (isDigitA != isDigitB)
-            {
-                return cmp.Compare(strA, iA, strB, iB, options);
-            }
-            else if (!isDigitA && !isDigitB)
-            {
-                int jA = iA + 1;
-                int jB = iB + 1;
-                while (jA < strA.Length && !Char.IsDigit(strA[jA])) jA++;
-                while (jB < strB.Length && !Char.IsDigit(strB[jB])) jB++;
-                int cmpResult = cmp.Compare(strA, iA, jA - iA, strB, iB, jB - iB, options);
-                if (cmpResult != 0)
-                {
-                    // Certain strings may be considered different due to "soft" differences that are
-                    // ignored if more significant differences follow, e.g. a hyphen only affects the
-                    // comparison if no other differences follow
-                    string sectionA = strA.Substring(iA, jA - iA);
-                    string sectionB = strB.Substring(iB, jB - iB);
-                    if (cmp.Compare(sectionA + "1", sectionB + "2", options) ==
-                        cmp.Compare(sectionA + "2", sectionB + "1", options))
-                    {
-                        return cmp.Compare(strA, iA, strB, iB, options);
-                    }
-                    else if (softResultWeight < 1)
-                    {
-                        softResult = cmpResult;
-                        softResultWeight = 1;
-                    }
-                }
-                iA = jA;
-                iB = jB;
-            }
-            else
-            {
-                char zeroA = (char)(strA[iA] - (int)Char.GetNumericValue(strA[iA]));
-                char zeroB = (char)(strB[iB] - (int)Char.GetNumericValue(strB[iB]));
-                int jA = iA;
-                int jB = iB;
-                while (jA < strA.Length && strA[jA] == zeroA) jA++;
-                while (jB < strB.Length && strB[jB] == zeroB) jB++;
-                int resultIfSameLength = 0;
-                do
-                {
-                    isDigitA = jA < strA.Length && Char.IsDigit(strA[jA]);
-                    isDigitB = jB < strB.Length && Char.IsDigit(strB[jB]);
-                    int numA = isDigitA ? (int)Char.GetNumericValue(strA[jA]) : 0;
-                    int numB = isDigitB ? (int)Char.GetNumericValue(strB[jB]) : 0;
-                    if (isDigitA && (char)(strA[jA] - numA) != zeroA) isDigitA = false;
-                    if (isDigitB && (char)(strB[jB] - numB) != zeroB) isDigitB = false;
-                    if (isDigitA && isDigitB)
-                    {
-                        if (numA != numB && resultIfSameLength == 0)
-                        {
-                            resultIfSameLength = numA < numB ? -1 : 1;
-                        }
-                        jA++;
-                        jB++;
-                    }
-                }
-                while (isDigitA && isDigitB);
-                if (isDigitA != isDigitB)
-                {
-                    // One number has more digits than the other (ignoring leading zeros) - the longer
-                    // number must be larger
-                    return isDigitA ? 1 : -1;
-                }
-                else if (resultIfSameLength != 0)
-                {
-                    // Both numbers are the same length (ignoring leading zeros) and at least one of
-                    // the digits differed - the first difference determines the result
-                    return resultIfSameLength;
-                }
-                int lA = jA - iA;
-                int lB = jB - iB;
-                if (lA != lB)
-                {
-                    // Both numbers are equivalent but one has more leading zeros
-                    return lA > lB ? -1 : 1;
-                }
-                else if (zeroA != zeroB && softResultWeight < 2)
-                {
-                    softResult = cmp.Compare(strA, iA, 1, strB, iB, 1, options);
-                    softResultWeight = 2;
-                }
-                iA = jA;
-                iB = jB;
-            }
-        }
-        if (iA < strA.Length || iB < strB.Length)
-        {
-            return iA < strA.Length ? 1 : -1;
-        }
-        else if (softResult != 0)
-        {
-            return softResult;
-        }
-        return 0;
-    }
-
-    public class CustomComparer<T> : IComparer<T>
-    {
-        private readonly Comparison<T> _comparison;
-
-        public CustomComparer(Comparison<T> comparison)
-        {
-            _comparison = comparison;
-        }
-
-        public int Compare(T? x, T? y)
-        {
-            if (x == null && y == null)
-            {
-                return 0;
-            }
-
-            if (x == null)
-            {
-                return -1;
-            }
-
-            if (y == null)
-            {
-                return 1;
-            }
-
-            return _comparison(x, y);
         }
     }
 }
