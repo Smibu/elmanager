25b7f604af75a4f6d4ea3159c76fde7ff9ac423f
Smibu <smibu90@gmail.com>
2026-08-24 18:18:49 +0300

    Fix viewport tearing due to hidden loading animation causing re-renders

--- Elmanager.SLE/UI/Spinner.axaml.cs
+++ Elmanager.SLE/UI/Spinner.axaml.cs
@@ -1,8 +1,18 @@
+using Avalonia;
 using Avalonia.Controls;
 
 namespace Elmanager.SLE.UI;
 
 internal partial class Spinner : UserControl
 {
+    public static readonly StyledProperty<double> RotationProperty =
+        AvaloniaProperty.Register<Spinner, double>(nameof(Rotation));
+
+    public double Rotation
+    {
+        get => GetValue(RotationProperty);
+        set => SetValue(RotationProperty, value);
+    }
+
     public Spinner() => InitializeComponent();
 }
